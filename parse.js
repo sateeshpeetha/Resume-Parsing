39da99b1443cf44e613ed1d29ebf27334bf74305
Sateesh Peetha <sateesh_peetha@yahoo.com>
2017-07-10 21:57:09 +0530

    added components shell code for each section of the profile

--- parse.js
+++ parse.js
@@ -3,10 +3,64 @@ function headerCtl($scope, $element, $attrs) {
   //add code later
    
 }
+
+
+function SummaryCtl($scope, $element, $attrs) {
+  //add code later
+   
+}
+
+
+function educationCtl($scope, $element, $attrs) {
+  //add code later
+   
+}
+
+
+function skillsCtl($scope, $element, $attrs) {
+  //add code later
+   
+}
+
+
+function expCtl($scope, $element, $attrs) {
+  //add code later
+   
+}
+
+
    
 app.component('header', {
 
   templateUrl: 'Candidate.tpl',
   controller: headerCtl
+
+});
+
+app.component('summary', {
+
+  templateUrl: 'Summary.tpl',
+  controller: SummaryCtl
+
+});
+
+app.component('education', {
+
+  templateUrl: 'Education.tpl',
+  controller: educationCtl
+
+});
+
+app.component('skills', {
+
+  templateUrl: 'Skills.tpl',
+  controller: skillsCtl
+
+});
+
+app.component('exp', {
+
+  templateUrl: 'Exp.tpl',
+  controller: expCtl
 
 });
